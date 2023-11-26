b09bbbded7081a9820119a259a964899153b5b10
Mehmet Deniz Birlikci <mdbirlikci@gmail.com>
2023-11-25 20:06:08 -0500

    Retrieval forward functions

--- llama/llama_adapter.py
+++ llama/llama_adapter.py
@@ -183,6 +183,50 @@ class LLaMA_adapter(nn.Module):
             c_loss = self.criterion(output.reshape(-1, self.llama.vocab_size), labels.flatten())
 
         return c_loss, c_loss
+    
+    def forward_retrieval(self, tokens, imgs):
+        visual_query = self.forward_visual(imgs)
+
+        _bsz, seqlen = tokens.shape
+
+        h = self.llama.tok_embeddings(tokens)
+        freqs_cis = self.llama.freqs_cis.to(h.device)
+        freqs_cis = freqs_cis[:seqlen]
+        mask = None
+        mask = torch.full((1, 1, seqlen, seqlen), float("-inf"), device=h.device)
+        mask = torch.triu(mask, diagonal=0 + 1).type_as(h)
+
+        for layer in self.llama.layers[:-1 * self.query_layer]:
+            h = layer(h, 0, freqs_cis, mask)
+
+        adapter = self.adapter_query.weight.reshape(self.query_layer, self.query_len, -1).unsqueeze(1)
+        adapter_index = 0
+        for layer in self.llama.layers[-1 * self.query_layer:]:
+            dynamic_adapter = adapter[adapter_index].repeat(_bsz, 1, 1)
+            dynamic_adapter = dynamic_adapter + visual_query
+            h = layer(h, 0, freqs_cis, mask, dynamic_adapter)
+            adapter_index = adapter_index + 1
+
+        h = self.llama.norm(h)
+        output = self.llama.out_retrieval(h[:, -1, :])
+        return output
+
+    def forward_retrieval_text(self, tokens):
+        _bsz, seqlen = tokens.shape
+
+        h = self.llama.tok_embeddings(tokens)
+        freqs_cis = self.llama.freqs_cis.to(h.device)
+        freqs_cis = freqs_cis[:seqlen]
+        mask = None
+        mask = torch.full((1, 1, seqlen, seqlen), float("-inf"), device=h.device)
+        mask = torch.triu(mask, diagonal=0 + 1).type_as(h)
+
+        for layer in self.llama.layers:
+            h = layer(h, 0, freqs_cis, mask)
+
+        h = self.llama.norm(h)
+        output = self.llama.out_retrieval(h[:, -1, :])
+        return output
 
     @torch.inference_mode()
     def forward_inference(self, visual_query, tokens, start_pos: int):
